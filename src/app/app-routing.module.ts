545da69f7da471acb2bf99e95e4d33abdf25457f
Juan David iron-mind <davidtovarmontoya@gmail.com>
2022-08-03 17:11:38 -0500

    services, navigation and links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+
+//creo la ruta que voy a usar
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'heroes', component: HeroesComponent },
+  {path:'dashboard', component:DashboardComponent},
+  { path: 'detail/:id', component: HeroDetailComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule
+  ],
+  //pa que sea posible a lo largo de toda la app
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,22 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
-  hero: Hero = {
-    name: 'Windstorm',
-    id: 1,
-  };
-  constructor() {}
+  heroes: Hero[] = [];
 
-  ngOnInit(): void {}
-  selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
+  // hero: Hero = {
+  //   name: 'Windstorm',
+  //   id: 1,
+  // };
+  // selectedHero: Hero={
+  //   name:'',
+  //   id:0
+  // };
+  // //Anido el servicio a mi componente 
+  // constructor(private heroService: HeroService,  private messageService: MessageService) {}
+  
+  // //NO OBSERVABLE
+  // // getHeroes(): void {
+  // //   this.heroes = this.heroService.getHeroes();
+  // // }
+
+  // //OBSERVABLE
+  // getHeroes(): void {
+  //   this.heroService.getHeroes()
+  //       .subscribe(heroes => this.heroes = heroes);
+  // }
+  // ngOnInit(): void {
+  //   //this.heroes = this.heroService.getHeroes() NO
+
+  //   //recomendable para iniciar la app (sirve para codigo asincoro)
+  //   this.getHeroes() //traigo los heros al montarse el componente
+  // }
+  
+  // onSelect(hero: Hero): void {
+    
+  //   this.selectedHero = hero;
+  //   this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  // }
 }
