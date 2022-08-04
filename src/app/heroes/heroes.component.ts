50096b9bff18a00e701ba65de076232bd5a7a02b
Juan david <davidtovarmontoya@gmail.com>
2022-08-04 14:53:35 -0500

    https

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,7 +11,7 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
-  
+
   constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
@@ -30,9 +30,9 @@ export class HeroesComponent implements OnInit {
   //   name:'',
   //   id:0
   // };
-  // //Anido el servicio a mi componente 
+  // //Anido el servicio a mi componente
   // constructor(private heroService: HeroService,  private messageService: MessageService) {}
-  
+
   // //NO OBSERVABLE
   // // getHeroes(): void {
   // //   this.heroes = this.heroService.getHeroes();
@@ -49,9 +49,9 @@ export class HeroesComponent implements OnInit {
   //   //recomendable para iniciar la app (sirve para codigo asincoro)
   //   this.getHeroes() //traigo los heros al montarse el componente
   // }
-  
+
   // onSelect(hero: Hero): void {
-    
+
   //   this.selectedHero = hero;
   //   this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   // }
